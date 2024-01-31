ee30502473488d9bd4ef4cdb6d26e87a3e68edec
Bryan-Herrera-Dev <hbryanelias@gmail.com>
2024-01-31 06:20:38 +0500

    fix: soluciona error con localIP cuando no esta configurado el eth0

--- ghost-rice/bspwm/scripts/localIP.sh
+++ ghost-rice/bspwm/scripts/localIP.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo "$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')"
+# Ejecuta ifconfig, redirige errores a /dev/null, y guarda el resultado
+ip=$(/usr/sbin/ifconfig eth0 2>/dev/null | grep "inet " | awk '{print $2}')
+
+# Verifica si la cadena de comandos fue exitosa
+if [ $? -eq 0 ] && [ -n "$ip" ]; then
+    echo "$ip"
+else
+    echo "0.0.0.0"
+fi

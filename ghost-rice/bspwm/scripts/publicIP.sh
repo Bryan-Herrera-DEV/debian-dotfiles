9e930ff1761045c93572a0c7d4103f48750f58d7
Bryan-Herrera-Dev <hbryanelias@gmail.com>
2024-01-31 06:18:40 +0500

    fix: soluciona error conek publicIp cuando obtiene una IPV4 y no IPV6

--- ghost-rice/bspwm/scripts/publicIP.sh
+++ ghost-rice/bspwm/scripts/publicIP.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "$(curl --silent ifconfig.io)"
+# Ejecuta curl y guarda el resultado
+output=$(curl --silent ipv4.icanhazip.com)
+
+# Define un patrón para una dirección IPv4 (formato básico)
+ipv4_pattern='[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+
+# Verifica si curl se ejecutó exitosamente y si la salida coincide con el patrón de una dirección IPv4
+if [ $? -eq 0 ] && [[ $output =~ $ipv4_pattern ]]; then
+    echo "$output"
+else
+    echo "192.168.1.1"
+fi
 
